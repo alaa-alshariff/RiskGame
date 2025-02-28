f8127abff24dc6e8dfe850c4f65e7db2678d6031
andreaboutros <andrea.boutros.a@gmail.com>
2025-02-27 20:37:41 -0500

    fix: switch to maven project to add testing libraries

---
+++ target/classes/Models/Player.java
@@ -0,0 +1,139 @@
+package Models;
+
+import Resources.Commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static View.GameMenu.SCANNER;
+
+
+/**
+ * This class describes information about each player.
+ */
+public class Player {
+
+    private String d_playerName;
+    List<Country> d_playerCountries;
+    List<Continent> d_playerContinents;
+
+    /**
+     * number of player's armies
+     */
+    Integer d_numOfReinforcements;
+
+    /**
+     * List of player's orders for execution.
+     */
+    List<Orders> d_playerOrders;
+    /**
+     * This is the constructor method of the Models.Player class
+     *
+     * @param v_playerName is player's name.
+     */
+    public Player(String v_playerName) {
+        this.d_playerName = v_playerName;
+        this.d_numOfReinforcements = Integer.valueOf(0);
+        this.d_playerOrders = new ArrayList<Orders>();
+        this.d_playerCountries = new ArrayList<Country>();
+        this.d_playerContinents = new ArrayList<Continent>();
+    }
+
+    public String get_playerName() {
+        return d_playerName;
+    }
+
+    public void set_playerName(String p_name) {
+        this.d_playerName = p_name;
+    }
+
+    public List<Country> get_playerCountries() {
+        return d_playerCountries;
+    }
+
+    public void set_playerCountries(List<Country> p_playerCountries) {
+        this.d_playerCountries = p_playerCountries;
+    }
+
+    public List<Continent> get_playerContinents() {
+        return d_playerContinents;
+    }
+
+    public void set_playerContinents(List<Continent> p_playerContinents) {
+        this.d_playerContinents = p_playerContinents;
+    }
+
+    public List<Orders> get_playerOrder() {
+        return d_playerOrders;
+    }
+
+    public void set_playerOrder(List<Orders> p_playerOrder) {
+        this.d_playerOrders = p_playerOrder;
+    }
+
+    public Integer get_armiesNumber() {
+        return d_numOfReinforcements;
+    }
+
+    public void set_armiesNumber(Integer p_armiesNumber) {
+        this.d_numOfReinforcements = p_armiesNumber;
+    }
+
+    /**
+     *“issue_order()” (no parameters, no return value) whose function is
+     *to add an order to the list of orders held by the
+     *player when the game engine calls it during the issue orders phase.
+     */
+    public void issue_order(){
+        while (d_numOfReinforcements != 0){
+            int l_countryID;
+            int l_numOfArmies;
+            System.out.println("Please issue deploy order command for Player " + d_playerName);
+            System.out.println("Remaining reinforcements: " + d_numOfReinforcements);
+            String l_command = SCANNER.nextLine();
+            String[] l_commandTokens = l_command.split(" ");
+            if (l_commandTokens.length != 3 || !l_commandTokens[0].equals(Commands.DEPLOY_COMMAND)){
+                System.out.println("Please give the command in format: " + Commands.DEPLOY_COMMAND_SYNTAX);
+                continue;
+            }
+            try {
+                l_countryID = Integer.parseInt(l_commandTokens[1]);
+                l_numOfArmies = Integer.parseInt(l_commandTokens[2]);
+            }catch (NumberFormatException e){
+                System.out.println("Invalid CountryID or Number of Reinforcements");
+                continue;
+            }
+            if (l_numOfArmies > d_numOfReinforcements){
+                System.out.println("Specified number of reinforcements exceed the available.");
+                continue;
+            }
+            boolean l_countryExists = false;
+            for (Country country : d_playerCountries)
+                if (country.d_countryID == l_countryID)
+                    l_countryExists = true;
+            if (!l_countryExists){
+                System.out.println("The given CountryID is not under your control.");
+                continue;
+            }
+            Orders order = new Orders(l_numOfArmies, l_countryID);
+            d_playerOrders.add(order);
+            d_numOfReinforcements = d_numOfReinforcements - l_numOfArmies;
+            System.out.println("Order Issued Successfully.");
+        }
+    }
+
+
+    /** * The player class must also have a
+     * “next_order()” (no parameters) method that is called by the GameEngine
+     * during executing order phase and
+     * returns the first order in the player’s list of orders, then removes it from the list.
+     */
+    public Orders next_order() {
+        if (d_playerOrders.isEmpty()) {
+            return null; // or throw an exception if desired
+        }
+        Orders l_firstOrder = d_playerOrders.get(0);
+        d_playerOrders.remove(0);
+        return l_firstOrder;
+    }
+}
