923b434db3b693266e875c457aab193d6f012d63
Alaa Alsharif <alaa.alshariff@hotmail.com>
2025-03-01 12:49:04 -0500

    fix: minor code optimizations

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import View.GameEngine;
 
 import java.io.IOException;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner SCANNER;
     public static void main(String[] args) throws IOException {
         GameEngine new_game = new GameEngine();
         new_game.start_game();
