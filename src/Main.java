5e5e8e10ad0168022bd9cceb5d50104fd49e4641
Alaa Alsharif <alaa.alshariff@hotmail.com>
2025-03-01 13:14:51 -0500

    remove unnecessary scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import Controller.GameEngine;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
-    public static Scanner SCANNER;
+
     public static void main(String[] args) throws IOException {
         GameEngine new_game = new GameEngine();
         new_game.start_game();
