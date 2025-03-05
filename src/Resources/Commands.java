b646275b96bc2e5c85adb9295f863de8627f893e
andreaboutros <andrea.boutros.a@gmail.com>
2025-03-04 22:11:54 -0500

    add/remove player input change

--- src/Resources/Commands.java
+++ src/Resources/Commands.java
@@ -2,7 +2,6 @@ package Resources;
 
 /**
  * This class is used to store all the possible commands and some syntax.
- *
  */
 public class Commands {
     public static final String LOAD_MAP_COMMAND = "loadmap";
@@ -12,6 +11,7 @@ public class Commands {
     public static final String EDIT_MAP_COMMAND = "editmap";
     public static final String VALIDATE_MAP_COMMAND = "validatemap";
     public static final String ASSIGN_COUNTRIES_COMMAND = "assigncountries";
+    public static final String PLAYER_EDIT_COMMAND = "gameplayer";
     public static final String PLAYER_ADD_COMMAND = "gameplayer -add";
     public static final String PLAYER_REMOVE_COMMAND = "gameplayer -remove";
     public static final String EDIT_CONTINENT_COMMAND = "editcontinent";
