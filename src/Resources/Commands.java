a64ad2da2372de66257cc47fe2c40d7bab6ba0e6
MasoumehF <masoumehfarokhpour.career@gmail.com>
2025-03-04 19:56:35 -0500

    adding map destination folder

--- src/Resources/Commands.java
+++ src/Resources/Commands.java
@@ -2,8 +2,8 @@ package Resources;
 
 /**
  * This class is used to store all the possible commands and some syntax.
+ *
  */
-
 public class Commands {
     public static final String LOAD_MAP_COMMAND = "loadmap";
     public static final String SHOW_MAP_COMMAND = "showmap";
@@ -19,4 +19,5 @@ public class Commands {
     public static final String EDIT_NEIGHBOR_COMMAND = "editneighbor";
     public static final String DEPLOY_COMMAND = "deploy";
     public static final String DEPLOY_COMMAND_SYNTAX = "deploy <countryID> <num>";
+    public static final String MAPS_DIRECTORY_PATH = "src/Resources/Maps";
 }
